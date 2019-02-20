7e5e9d08011d6f363c315aa8ebaac298e5912d2c
guoxiujun <guoxiujun@beyondcent.com>
2019-02-20 10:49:58 +0800

    feature:测试功能增加
    增加自动登陆爬虫LoginSpider 未测试
    setting文件修改，允许存储cookie

--- MoonSpider/items.py
+++ MoonSpider/items.py
@@ -6,12 +6,14 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.item import Item,Field
+from scrapy.item import Item, Field
+
 
 class MoonspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
 
 class ArticleSpiderItem(scrapy.Item):
     title = Field()
@@ -19,3 +21,13 @@ class ArticleSpiderItem(scrapy.Item):
     recent = Field()
     article = Field()
     domain = Field()
+
+
+class ZhihuItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    url = Field()  # 保存抓取问题的url
+    title = Field()  # 抓取问题的标题
+    description = Field()  # 抓取问题的描述
+    answer = Field()  # 抓取问题的答案
+    name = Field()  # 个人用户的名称
--- MoonSpider/spiders/ArticleSpider.py
+++ MoonSpider/spiders/ArticleSpider.py
@@ -7,16 +7,18 @@ class ArticleSpider(Spider):
     name = "article_robot"
     start_urls = ["https://www.biqiuge.com/book/4772/"]
     domain_name = "https://www.biqiuge.com"
+    allowed_domains = ['www.biqiuge.com']
+
 
     def parse(self, response):
         items = []
-        recentArticles = response.xpath('//div[@class="listmain"]/dl/dd[position()<6]/a')
-        allArticles = response.xpath('//div[@class="listmain"]/dl/dd[position()>6]/a')
-        self.extract_item(items, recentArticles, True)
-        self.extract_item(items, allArticles, False)
+        recent_articles = response.xpath('//div[@class="listmain"]/dl/dd[position()<6]/a')
+        all_articles = response.xpath('//div[@class="listmain"]/dl/dd[position()>6]/a')
+        self.extract_item(items, recent_articles, True)
+        self.extract_item(items, all_articles)
         return items
 
-    def extract_item(self, items, articles, recent):
+    def extract_item(self, items, articles, recent=False):
         for title in articles:
             item = ArticleSpiderItem()
             item['title'] = title.xpath("text()").extract()[0].strip()
